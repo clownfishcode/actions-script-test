52e1cbd135fb166418d957389ac5534014f27773
liuziyuan <liuziyuan.room@gmail.com>
2024-08-08 18:03:01 +0800

    add

--- src/main/java/GithubActionsMavenScriptTest.java
+++ src/main/java/GithubActionsMavenScriptTest.java
@@ -2,4 +2,8 @@ public class GithubActionsMavenScriptTest {
     public void test() {
         System.out.println("Hello World!");
     }
+
+    public void test2() {
+        System.out.println("Hello World2!");
+    }
 }
